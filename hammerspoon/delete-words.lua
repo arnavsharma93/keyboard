8e2c39a5df0ad22a9e5c7e1d9468fdd0bc2eb347
Arnav Sharma <arnav@uber.com>
2019-01-01 23:00:35 +0530

    Removing `ctrul-u` mapping

--- hammerspoon/delete-words.lua
+++ hammerspoon/delete-words.lua
@@ -31,7 +31,7 @@ end)
 -- enabled in the terminal, it would break the standard control + u behavior.
 -- Therefore, we only enable this hotkey for non-terminal apps.
 local wf = hs.window.filter.new():setFilters({iTerm2 = false, Terminal = false})
-enableHotkeyForWindowsMatchingFilter(wf, hs.hotkey.new({'ctrl'}, 'u', function()
+enableHotkeyForWindowsMatchingFilter(wf, hs.hotkey.new({'alt'}, 'u', function()
   keyUpDown({'cmd'}, 'delete')
 end))
 
