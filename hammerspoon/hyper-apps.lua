041ce4315739b816ecb2cb0060a853f0aa994363
Arnav Sharma <arnav@uber.com>
2019-01-01 23:42:31 +0530

    Adding app shortcuts using hammerspoon

---
+++ hammerspoon/hyper-apps.lua
@@ -0,0 +1,14 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 's', 'Spotify' },
+  { 'g', 'Google Chrome' },
+  { 'u', 'uChat' },
+  { 'e', 'emacs' },
+  { 'f', 'Finder' },
+  { 't', 'iTerm' },
+  { 'n', 'Evernote' },
+}
